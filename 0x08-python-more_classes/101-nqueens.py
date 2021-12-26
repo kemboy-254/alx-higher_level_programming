ef8401990d069a6af7cd65399b2e115a2f1f32da
Michael Kipkemboy <kipkemboymichael05@gmail.com>
2021-12-26 22:36:59 +0300

    Meet pep8 requirements

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -21,6 +21,7 @@ queen must be placed on the chessboard.
 """
 import sys
 
+
 def init_board(n):
     """Initialize an `n`x`n` sized chessboard with 0's."""
     board = []
@@ -28,11 +29,13 @@ def init_board(n):
     [row.append(' ') for i in range(n) for row in board]
     return (board)
 
+
 def board_deepcopy(board):
     """Return a deepcopy of a chessboard."""
     if isinstance(board, list):
         return list(map(board_deepcopy, board))
     return (board)
+
 
 def get_solution(board):
     """Return the list of lists representation of a solved chessboard."""
