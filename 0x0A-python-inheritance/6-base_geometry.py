932c248c745a2b45ec6e2eced398af5abe7500f1
Michael Kipkemboy <kipkemboymichael05@gmail.com>
2022-09-27 00:35:08 +0300

    Add all Python Inheritance files

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" File name : 6-base_geometry.py
+    It is not allowed to import any module
+"""
+
+
+class BaseGeometry(object):
+    """Define class base geometry"""
+    def area(self):
+        """area: return area"""
+        raise Exception("area() is not implemented")
