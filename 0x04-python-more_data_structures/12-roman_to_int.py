33cb7b2b8894a9b998a7457020dfac36fb321416
Michael Kipkemboy <kipkemboymichael05@gmail.com>
2022-09-27 00:43:02 +0300

    Add missing files

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -12,7 +12,7 @@ def roman_to_int(roman_string):
             "C": 100,
             "D": 500,
             "M": 1000
-    }
+            }
     num = 0
 
     for i in range(len(roman_string)):
@@ -21,7 +21,7 @@ def roman_to_int(roman_string):
 
         if (i != (len(roman_string) - 1) and
                 roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-                num += roman_dict[roman_string[i]] * -1
+            num += roman_dict[roman_string[i]] * -1
 
         else:
             num += roman_dict[roman_string[i]]
